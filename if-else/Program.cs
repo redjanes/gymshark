acf07cbb9a2024e82da3c297e72eec384e15a165
redjanes <119490756+redjanes@users.noreply.github.com>
2022-12-06 11:08:04 +0300

    Add files via upload

---
+++ if-else/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class program
+
+{
+    static void Main(string[]args)
+    {
+        int time=DateTime.Now.Hour;
+
+        if(time>=6)
+        Console.WriteLine("gunaydin!");
+        else if(time<=18)
+        Console.WriteLine("iyi gunler!");
+        else
+        Console.WriteLine("iyi geceler!");
+
+        string sonuc = time<=18 ? "iyi gunler!": "iyi geceler";
+        sonuc=time>=6 && time<11 ? "günaydin": time<=18 ? "iyi günler!": "iyi geceler!";
+        Console.WriteLine(sonuc);
+
+    }
+    
+    
+}
+
+
+
+  
