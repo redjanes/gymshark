d0ca9f3b6bc6df05aece0f5b88cd37b68609b72e
redjanes <119490756+redjanes@users.noreply.github.com>
2022-12-06 16:45:26 +0300

    Add files via upload

---
+++ donguler 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+ class Program
+
+   
+        {
+            static void Main(string[] args)
+            {
+                //while
+                //1 den başlayarak consoleden girilen sayıya kadar (sayi dahil) ortalama hesaplayıp console yazdıran program
+                Console.Write("lutfen bir sayi giriniz:");
+                int sayi = int.Parse(Console.ReadLine());
+                int sayac = 1;
+                int toplam = 0;
+                while (sayac <= sayi)
+                {
+                    toplam += sayac;
+                    sayac++;
+                }
+                Console.WriteLine(toplam/sayi);
+                
+
+                // 'a' dan 'z'ye kadar tüm harfeleri console a yazdırır.
+                char character='a';
+                while(character<'z')
+                {
+                    Console.Write(character);
+                    character++;
+                }                
+                Console.Write("***********foreach********");
+                string[] arabalar={"bmw","ford","toyota","nissan"};
+                foreach(var araba in arabalar)
+                {
+                    Console.WriteLine(araba);
+
+                }
+
+
+
+
+
+            }
+
+
+
+
+     }   
